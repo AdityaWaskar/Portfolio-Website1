aab2473f819d6b83aae604a4ef4bea573836c256
Aditya Waskar <adityawaskar05@gmail.com>
2023-05-29 17:09:37 +0530

    add download resume functionality

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,21 @@
 import "./intro.css";
 import Typical from "react-typical";
 
+const onButtonClick = () => {
+  // using Java Script method to get PDF file
+  fetch("resume.pdf").then((response) => {
+    response.blob().then((blob) => {
+      // Creating new object of PDF file
+      const fileURL = window.URL.createObjectURL(blob);
+      // Setting various property values
+      let alink = document.createElement("a");
+      alink.href = fileURL;
+      alink.download = "adityawaskar.pdf";
+      alink.click();
+    });
+  });
+};
+
 const Intro = () => {
   return (
     <div className="intro" id="intro">
@@ -30,7 +45,7 @@ const Intro = () => {
             />
           </h3>
           <div className="buttons">
-            <button>Download CV</button>
+            <button onClick={onButtonClick}>Download CV</button>
             <button>Let's Talk</button>
           </div>
         </div>
